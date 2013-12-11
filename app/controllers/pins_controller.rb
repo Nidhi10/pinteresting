e8637a0f561cfa10d82716300fc1c33971de07c2
Nidhi Mittal <nidhi105@gmail.com>
2013-12-11 13:36:49 -0500

    added pins scaffold and Paperclip image upload

---
+++ app/controllers/pins_controller.rb
@@ -0,0 +1,83 @@
+class PinsController < ApplicationController
+  before_action :authenticate_user!, except: [:show,:index]
+  before_action :set_pin, only: [:show]
+  before_action :correct_pin, only: [:edit,:update,:destroy]
+
+  # GET /pins
+  # GET /pins.json
+  def index
+    @pins = Pin.all
+  end
+
+  # GET /pins/1
+  # GET /pins/1.json
+  def show
+  end
+
+  # GET /pins/new
+  def new
+    @pin = current_user.pins.build
+  end
+
+  # GET /pins/1/edit
+  def edit
+  end
+
+  # POST /pins
+  # POST /pins.json
+  def create
+    @pin = current_user.pins.build(pin_params)
+
+    respond_to do |format|
+      if @pin.save
+        format.html { redirect_to @pin, notice: 'Pin was successfully created.' }
+        format.json { render action: 'show', status: :created, location: @pin }
+      else
+        format.html { render action: 'new' }
+        format.json { render json: @pin.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /pins/1
+  # PATCH/PUT /pins/1.json
+  def update
+    respond_to do |format|
+      if @pin.update(pin_params)
+        format.html { redirect_to @pin, notice: 'Pin was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: 'edit' }
+        format.json { render json: @pin.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /pins/1
+  # DELETE /pins/1.json
+  def destroy
+    @pin.destroy
+    respond_to do |format|
+      format.html { redirect_to pins_url }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_pin
+      @pin = Pin.find(params[:id])
+    end
+
+    def correct_pin
+      @pin = current_user.pins.find_by(id: params[:id])
+      if @pin == nil
+        redirect_to pins_path,notice:"You are not authorized to change this pin"
+      end
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def pin_params
+      params.require(:pin).permit(:description,:image)
+    end
+end
